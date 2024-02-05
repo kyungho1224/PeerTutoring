a02cf513b32a1790318247fe1b136506e27034f0
kimkyungho <dev_kimkyungho@kakao.com>
2024-02-05 17:22:37 +0900

    PeerTutoring-240205

---
+++ Java001/src/main/java/entity/People.java
@@ -0,0 +1,9 @@
+package entity;
+
+/**
+ * Created by KimKyungHo on 2024-02-05(005)
+ */
+public class People {
+    private Integer id;
+    private String name;
+}
---
+++ Java001/src/main/java/entity/Student.java
@@ -0,0 +1,10 @@
+package entity;
+
+/**
+ * Created by KimKyungHo on 2024-02-05(005)
+ */
+public class Student {
+    private Double score;
+    private String grade;   // A, B, C, D, F
+
+}
