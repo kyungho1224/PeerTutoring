ceb6d44d49c802f46c5530e72bfdd23b112a4fae
kimkyungho <dev_kimkyungho@kakao.com>
2024-02-07 14:01:48 +0900

    Programmer.java 접근제어자 변경

--- Java002/src/main/java/entity/Programmer.java
+++ Java002/src/main/java/entity/Programmer.java
@@ -3,7 +3,7 @@ package entity;
 /**
  * Created by KimKyungHo on 2024-02-07(007)
  */
-public class Programmer {
+abstract class Programmer {
 
     public void display() {
         System.out.println("개발자입니다.");
